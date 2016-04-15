76e7e1845098519e6f9237af6bd722901b955225
leandromoh <leandromoh@hotmail.com>
2016-04-15 00:07:29 -0300

    concertado bug no onclick da div do test.html

--- js/exec test v1.0.js
+++ js/exec test v1.0.js
@@ -1,28 +1,32 @@
-function uuu(obj){
-	pre.innerHTML = obj.value;
-	pre.onclick();
-}
-
 document.addEventListener("DOMContentLoaded", function(event) { 
 	textarea = document.getElementById("textarea");
 	pre = document.getElementById("csharpcode");
-	
-	textarea.setAttribute("onkeyup","uuu(this)");
-	pre.setAttribute("onclick","applySyntaxHighlighter(this)");
+	auxDiv = document.getElementById("aux");
 
-	textarea.onkeyup();
-	
-	var spaces = "    ";
+	textarea.onkeyup = function(){
+        pre.innerHTML = this.value;
+        auxDiv.onclick();
+    }
+    
+	auxDiv.onclick = function(){
+        applySyntaxHighlighter(pre);
+    };
+
+    textarea.onkeyup();
+    
+    function onkeydownWithTab(e){
+        if(e.keyCode == 9 || e.which == 9){
+            e.preventDefault();
+            var s = this.selectionStart;
+            this.value = this.value.substring(0,this.selectionStart) + "    " + this.value.substring(this.selectionEnd);
+            this.selectionEnd = s + "    ".length; 
+        }
+    }
+    
 	var textareas = document.getElementsByTagName('textarea');
-	var count = textareas.length;
-	for(var i=0;i<count;i++){
-		textareas[i].onkeydown = function(e){
-			if(e.keyCode==9 || e.which==9){
-				e.preventDefault();
-				var s = this.selectionStart;
-				this.value = this.value.substring(0,this.selectionStart) + spaces + this.value.substring(this.selectionEnd);
-				this.selectionEnd = s + spaces.length; 
-			}
-		}
-	}
+	var i = textareas.length;
+    
+	while(i--){
+        textareas[i].onkeydown = onkeydownWithTab;
+    }
 });
